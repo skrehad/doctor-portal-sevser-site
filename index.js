2e208ba1604f1957e60d3e740cef45bb806c1437
rehadhasan <rehadhasan664@gmail.com>
2023-06-04 23:45:21 +0600

    added appointmentOptions routes for backend

--- index.js
+++ index.js
@@ -21,16 +21,15 @@ const client = new MongoClient(uri, {
 
 async function run() {
   try {
-    // Connect the client to the server	(optional starting in v4.7)
-    await client.connect();
-    // Send a ping to confirm a successful connection
-    await client.db("admin").command({ ping: 1 });
-    console.log(
-      "Pinged your deployment. You successfully connected to MongoDB!"
-    );
+    const appointmentOptionCollection = client
+      .db("doctor-portal")
+      .collection(appointmentOptions);
   } finally {
-    // Ensures that the client will close when you finish/error
-    await client.close();
+    app.get("/appointmentOption", async (req, res) => {
+      const query = {};
+      const options = await appointmentOptionCollection.find(query).toArray();
+      res.send(options);
+    });
   }
 }
 run().catch(console.dir);
