48d183d81b4e804e056643724fd674620180a9d9
rehadhasan <rehadhasan664@gmail.com>
2023-06-04 23:34:33 +0600

    update mongodb user and pass

--- index.js
+++ index.js
@@ -8,12 +8,8 @@ const port = process.env.PORT || 5000;
 app.use(cors());
 app.use(express.json());
 
-// doctor-portal
-// YoKoo6NPfpjeC6id
-
-const uri =
-  "mongodb+srv://<username>:<password>@cluster0.bwvhp.mongodb.net/?retryWrites=true&w=majority";
-
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.bwvhp.mongodb.net/?retryWrites=true&w=majority`;
+console.log(uri);
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
 const client = new MongoClient(uri, {
   serverApi: {
