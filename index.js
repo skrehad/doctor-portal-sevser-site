cde103a4712b897648094657df5708d9e6ce2a93
rehadhasan <rehadhasan664@gmail.com>
2023-06-04 16:16:33 +0600

    first commit

---
+++ index.js
@@ -0,0 +1,13 @@
+const express = require("express");
+const cors = require("cors");
+const app = express();
+const port = process.env.PORT || 5000;
+
+app.use(cors());
+app.use(express.json());
+
+app.get("/", async (req, res) => {
+  res.send("doctor portal running");
+});
+
+app.listen(port, () => console.log(`doctor portal running on ${port}`));
