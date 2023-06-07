4ca16101b944d3fbf37a813cbe58cbf401985396
rehadhasan <rehadhasan664@gmail.com>
2023-06-07 20:02:42 +0600

    update backend data

--- index.js
+++ index.js
@@ -9,7 +9,6 @@ app.use(cors());
 app.use(express.json());
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.bwvhp.mongodb.net/?retryWrites=true&w=majority`;
-console.log(uri);
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
 const client = new MongoClient(uri, {
   serverApi: {
@@ -23,13 +22,14 @@ async function run() {
   try {
     const appointmentOptionCollection = client
       .db("doctor-portal")
-      .collection(appointmentOptions);
-  } finally {
-    app.get("/appointmentOption", async (req, res) => {
+      .collection("appointmentOptions");
+
+    app.get("/appointmentOptions", async (req, res) => {
       const query = {};
       const options = await appointmentOptionCollection.find(query).toArray();
       res.send(options);
     });
+  } finally {
   }
 }
 run().catch(console.dir);
