7f245bc00862bef1ff07bb20a4bbfa00b100e4fc
rehadhasan <rehadhasan664@gmail.com>
2023-06-24 17:47:44 +0600

    added payment system in backend

--- index.js
+++ index.js
@@ -5,6 +5,7 @@ var jwt = require("jsonwebtoken");
 const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 require("dotenv").config();
 const port = process.env.PORT || 5000;
+const stripe = require("stripe")(process.env.STRIPE_SECRET_KEY);
 
 app.use(cors());
 app.use(express.json());
@@ -89,6 +90,7 @@ async function run() {
         return res.status(403).send({ message: "forbidden access" });
       }
       const query = { email: email };
+
       const bookings = await bookingCollection.find(query).toArray();
       res.send(bookings);
     });
@@ -110,6 +112,29 @@ async function run() {
       res.send(result);
     });
 
+    app.get("/bookingCollections/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) };
+      const booking = await bookingCollection.findOne(query);
+      res.send(booking);
+    });
+
+    // for payment system
+    app.post("/create-payment-intent", async (req, res) => {
+      const booking = req.body;
+      const price = booking.price;
+      const amount = price * 100;
+
+      // Create a PaymentIntent with the order amount and currency
+      const paymentIntent = await stripe.paymentIntents.create({
+        currency: "usd",
+        amount: amount,
+        payment_method_types: ["card"],
+      });
+      res.send({
+        clientSecret: paymentIntent.client_secret,
+      });
+    });
     // for get jwt token
     app.get("/jwt", async (req, res) => {
       const email = req.query.email;
@@ -159,6 +184,22 @@ async function run() {
       );
       res.send(result);
     });
+
+    // app.get("/addPrice", async (req, res) => {
+    //   const filter = {};
+    //   const options = { upsert: true };
+    //   const updateDoc = {
+    //     $set: {
+    //       price: "99",
+    //     },
+    //   };
+    //   const result = await appointmentOptionCollection.updateMany(
+    //     filter,
+    //     updateDoc,
+    //     options
+    //   );
+    //   res.send(result);
+    // });
 
     app.get("/appointmentSpecialty", async (req, res) => {
       const query = {};
